e3c7a545d37fcccc8a2721b76b1a0b6344623b17
andrewpap22 <andrewpap1997@gmail.com>
2020-12-15 23:07:55 +0200

    complete logistic regression in C++, need to modify it a bit to be C compatible

--- src/MachineLearning/include/ETL.h
+++
@@ -1,6 +0,0 @@
-#ifndef ETL_H
-#define ETL_H
-
-
-
-#endif /* ETL_H */
---
+++ src/MachineLearning/main_log_regr.cpp
@@ -0,0 +1,24 @@
+#include "include/Logistic_Regr.h"
+#include "./source/Logistic_Regr.cpp"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define features 4
+#define learning_rate 0.003
+#define iterations 8000
+
+int main(){
+	string train = "./data/train.txt";
+	string test = "./data/test.txt";
+
+	LogisticRegression lr(learning_rate, iterations);
+	Matrix training_exp = lr.readFromInputFile(train);
+	Matrix test_exp = lr.readFromInputFile(test);
+
+	double bias = 1;
+	Point4d weights = Point4d(features, 1);
+	Point5d optimal_weights = lr.gradientDesent(bias, weights, training_exp);
+	
+	lr.testAndPrint(optimal_weights, test_exp);
+}
